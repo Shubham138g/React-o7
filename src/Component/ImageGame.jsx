daaa75a23a062533db12ddda558abc9b42951fa6
shubham <tonygupta275@gmail.com>
2024-04-09 16:31:11 +0530

    making imageGame

--- src/Component/ImageGame.jsx
+++ src/Component/ImageGame.jsx
@@ -1,19 +1,25 @@
-import React, {useState, Fragment } from "react";
-import {toast} from "react-toastify";
+import React, { useState, Fragment } from "react";
+import { toast } from "react-toastify";
 
 export default function ImageGame() {
-    const  [number, setNumber] = useState("");
-    const  [result, setResult] = useState("Play");
-    const handleInput=(e)=>{
-        setNumber(e.target.value);
+  const [number, setNumber] = useState("");
+  const [result, setResult] = useState("Play");
+  const handleInput = (e) => {
+    setNumber(e.target.value);
+  };
+  const checkNumber = () => {
+    if (number <= 1 || number > 5) {
+      toast("number is not vaild");
+    }
+  };
+  const URL = [
+    "/image/1.jpg",
+    "/image/2.jpg",
+    "/image/3.jpg",
+    "/image/4.jpg",
+    "/image/5.jpg",
+  ];
 
-    }
-    const checkNumber=()=>{
-        if(number<=1 || number>5){
-            toast("number is not vaild")
-        }
-    }
-    
   return (
     <Fragment>
       <div className="container my-5">
@@ -27,14 +33,24 @@ export default function ImageGame() {
           min={2}
           max={5}
         />
-        <button  type="submit" onClick={checkNumber} className="ms-3 px-3 bg-dark text-light">Enter</button>
+        <button
+          type="submit"
+          onClick={checkNumber}
+          className="ms-3 px-3 bg-dark text-light"
+        >
+          Enter
+        </button>
         <div className="mt-2">
-            <small className="text-danger">Please choose number between 2 to 5</small>
+          <small className="text-danger">
+            Please choose number between 2 to 5
+          </small>
         </div>
         <div className="row">
-            <div className="col-3">
-                <img src="/image/1.jpg"/>
-            </div>
+          {URL.map((pic, index) => (
+            <div className="col-3 my-5">
+              <img width="250px" key={index} src={pic} alt={`Image ${index + 1}`} />
+            </div>  
+          ))}
         </div>
       </div>
     </Fragment>
